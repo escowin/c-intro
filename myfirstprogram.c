fa1ceaf3dc32d426f5ad42cc99b29f9b2208febb
escowin <edwin@escowinart.com>
2023-08-27 21:02:25 -0500

    set up basic C print function

---
+++ myfirstprogram.c
@@ -0,0 +1,10 @@
+// header file library | allows work with input & output functions
+#include <stdio.h>
+
+// function
+int main() {
+    // function | outputs text in terminal
+    printf("C terminal test");
+    // return ends the main() function
+    return 0;
+}
