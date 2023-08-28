0a1ead2795a205522eebbeec1fb8b2aed5ca2264
escowin <edwin@escowinart.com>
2023-08-27 23:14:27 -0500

    printed variable types using corresponding specifiers

--- myfirstprogram.c
+++ myfirstprogram.c
@@ -1,10 +1,17 @@
-// header file library | allows work with input & output functions
-#include <stdio.h>
+#include <stdio.h> // header file library | allows work with input & output functions
+// variable types | int, float, char
+// format specifiers | %d, %f, %c 
+int num = 12;
+float floatNum = -5.99;
+char charVal = 'X'; // must use single quotes
 
 // function
 int main() {
-    // function | outputs text in terminal w/ escape sequences
-    printf("Hello.\tI am learning how to \"C\"\nfor the first time.");
+    // outputs text in terminal w/ escape sequences
+    printf("I am learning how to \"C\"...\tfor the first time.\n");
+    printf("%d\n", num);
+    printf("%f\n", floatNum);
+    printf("%c\n", charVal);
     // return ends the main() function
     return 0;
 }
